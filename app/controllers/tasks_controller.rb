517cd71460ef69aa3a463ee24e4cb819433f4038
Rodrigo Lascano <rodrigo.lascano@gmail.com>
2018-03-08 20:40:47 -0300

    Can create new tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,25 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to 'index'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def task_params
+      params.require(:task).permit(:description, :completed)
+    end
+end
