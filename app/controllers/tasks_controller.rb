f083ec8615b8d1add57e295d881dd2914fa7747b
Rodrigo Lascano <rodrigo.lascano@gmail.com>
2018-03-12 23:57:12 -0300

    Done with basic functionality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,23 @@ class TasksController < ApplicationController
     end
   end
 
+  def update
+    @task = Task.find(params[:id])
+    @task.update_attributes!(task_params)
+    respond_to do |format|
+      format.html { redirect_to tasks_url }
+      format.js
+    end
+  end
+
+  def destroy
+    @task = Task.destroy(params[:id])
+    respond_to do |format|
+      format.html { redirect_to tasks_url }
+      format.js
+    end
+  end
+
   private
 
     def task_params
